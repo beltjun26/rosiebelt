5e0c92b853562fb115d1de0d3bd68ca6b80c7643
Rosiebelt Jun Abisado <beltjunabisado@gmail.com>
2022-03-24 14:14:26 +0800

    fix absolute import

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from 'App/Assets/logo/logo.svg';
 import './App.css';
 
-function App() {
+export const App: React.FC = ()  => {
   return (
     <div className="App">
       <header className="App-header">
@@ -22,5 +22,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
