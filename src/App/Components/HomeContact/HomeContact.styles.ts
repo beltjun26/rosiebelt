af552e7ec776fbab9da5057d0cbb84211cce5f72
Rosiebelt Jun Abisado <beltjunabisado@gmail.com>
2022-04-19 14:47:36 +0800

    center HomeContact component

--- src/App/Components/HomeContact/HomeContact.styles.ts
+++ src/App/Components/HomeContact/HomeContact.styles.ts
@@ -3,6 +3,10 @@ import styled from 'styled-components';
 export const HomeContact = styled.div`
   min-height: 100vh;
   background-color: rgba(239, 239, 239, 1);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
 `
 
 export const Header = styled.h1`
