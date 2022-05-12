8a4105f779e1aa74f709755d02f89a707b7727e1
Rosiebelt Jun Abisado <beltjunabisado@gmail.com>
2022-05-12 12:21:38 +0800

    remove uncessary code

--- src/App/Pages/Loading/Home/Home.styles.ts
+++ src/App/Pages/Loading/Home/Home.styles.ts
@@ -1,53 +1,5 @@
 import styled from "styled-components";
-// import { ReactComponent as MountainSingleSvg } from 'App/Assets/logo/mountainSingle.svg';
-// import { ReactComponent as MountainRangeSvg } from 'App/Assets/logo/mountainRange.svg';
 
 export const Home = styled.div`
   overflow: hidden;
 `;
-
-// export const HeaderContainer = styled.div`
-//   position: relative;
-//   color: rgba(111, 151, 181, 1);
-//   display: flex;
-//   flex-direction: column;
-//   align-items: center;
-// `
-
-// export const SubHeaderText = styled.div`
-//   font-size: 28px;
-//   font-style: normal;
-//   font-weight: 400;
-//   line-height: 28px;
-//   letter-spacing: 0.12em;
-//   margin-bottom: 1em;
-// `
-
-// export const HeaderText = styled.div`
-//   text-align: center;
-//   font-size: 2.5em;
-//   font-weight: 700;
-//   line-height: 1em;
-//   letter-spacing: 0.12em;
-//   margin-bottom: .3em;
-// `
-
-// export const Occupation = styled.div`
-//   font-size: 28px;
-//   font-weight: 400;
-//   line-height: 28px;
-//   letter-spacing: 0.12em;
-// `
-
-// export const MountainSingle = styled(MountainSingleSvg)`
-//   position: absolute;
-//   left: -30em;
-//   overflow: visible;
-// `
-
-// export const MountainRange = styled(MountainRangeSvg)`
-//   position: absolute;
-//   right: -55em;
-//   bottom: -30em;
-//   overflow: visible;
-// `
